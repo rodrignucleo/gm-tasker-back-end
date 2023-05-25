2b4b56d3fc2ff39f4cc17e291f79ace922da118f
Rodrigo Ribeiro <rodrignucleo@gmail.com>
2023-05-25 20:03:48 -0300

    add docker API

--- Sistema/GMTasker.RazorPages/Pages/Usuario/Index.cshtml.cs
+++ Sistema/GMTasker.RazorPages/Pages/Usuario/Index.cshtml.cs
@@ -16,7 +16,7 @@ namespace GMTasker.RazorPages.Pages.Usuario
         public async Task<IActionResult> OnGetAsync(){
             // GarconList = await _context.Garcon!.ToListAsync();
             var httpClient = new HttpClient();
-            var url = "http://localhost:5072/api/Usuario";
+            var url = "http://localhost:32770/api/Usuario";
             var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
             var response = await httpClient.SendAsync(requestMessage);
             var content = await response.Content.ReadAsStringAsync();
